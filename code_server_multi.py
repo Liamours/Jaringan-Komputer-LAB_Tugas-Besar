29f2516fbd739473fe8ccf9ff8383a15c2d01b85
Liamours <143605412+Liamours@users.noreply.github.com>
2025-05-14 12:06:00 +0700

    Add code

---
+++ code_server_multi.py
@@ -0,0 +1,75 @@
+"""
+Saat ini, server web hanya menangani satu request HTTP pada satu waktu. Implementasikan 
+sebuah server multithread yang mampu melayani beberapa requests secara simultan. Dengan 
+menggunakan  threading,  pertama-tama  buat  sebuah  thread  utama  di  mana  server  yang 
+dimodifikasi listens klien pada port tertentu. Ketika menerima request koneksi TCP dari 
+seorang  klien,  server  akan  menyiapkan  koneksi  TCP  melalui  port  lain  dan  melayani 
+permintaan klien dalam sebuah thread terpisah. Akan ada sebuah koneksi TCP terpisah dalam 
+sebuah utas terpisah untuk setiap pasangan permintaan/respons.
+"""
+
+from socket import *
+import threading
+import os
+
+def handle_client(connectionSocket):
+    try:
+        data = connectionSocket.recv(1024)
+        message = data.decode('utf-8', errors='ignore')
+
+        if not message.strip():
+            connectionSocket.close()
+            return
+
+        parts = message.split()
+        if len(parts) < 2:
+            connectionSocket.close()
+            return
+
+        filename = parts[1]
+
+        # Sanitize filename
+        if '\x00' in filename:
+            connectionSocket.close()
+            return
+
+        # Strip leading slash and prevent directory traversal
+        filename = filename.lstrip('/')
+        if '..' in filename or filename.startswith('/'):
+            connectionSocket.send("HTTP/1.1 403 Forbidden\r\n\r\n".encode())
+            connectionSocket.close()
+            return
+
+        # Open and read file
+        if not os.path.exists(filename):
+            raise IOError
+
+        with open(filename, encoding='utf-8') as f:
+            outputdata = f.read()
+
+        # Send HTTP response header
+        connectionSocket.send("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n".encode())
+
+        # Send file content
+        connectionSocket.sendall(outputdata.encode())
+
+    except IOError:
+        connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
+        connectionSocket.send("<html><body><h1>404 Not Found</h1></body></html>".encode())
+    finally:
+        connectionSocket.close()
+
+# Main server thread
+SERVER_HOST = '127.0.0.1'
+SERVER_PORT = 1234
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind((SERVER_HOST, SERVER_PORT))
+serverSocket.listen(5)
+
+print("Multithreaded server is ready to serve...")
+
+while True:
+    connectionSocket, addr = serverSocket.accept()
+    print(f"Connection from {addr}")
+    client_thread = threading.Thread(target=handle_client, args=(connectionSocket,))
+    client_thread.start()
--- code_server_single.py
+++ code_server_single.py
@@ -1,18 +1,26 @@
+"""
+Anda akan mengembangkan sebuah server web yang menangani satu request HTTP pada satu 
+waktu. Server web harus accept dan parse request HTTP, mendapatkan file yang diminta dari 
+sistem file server, membuat message response HTTP yang terdiri dari file yang requested yang 
+didahului oleh baris header, dan kemudian send response langsung ke klien. Jika file yang 
+diminta tidak ada di server, server harus mengirimkan message HTTP "404 Not Found" 
+kembali ke klien.
+"""
+
 from socket import *
-import sys 
 
 SERVER_HOST = '127.0.0.1'
 SERVER_PORT = 1234 
 serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind((SERVER_HOST, SERVER_PORT)) 
-serverSocket.listen(5) 
+serverSocket.bind((SERVER_HOST, SERVER_PORT))
+serverSocket.listen(1)
 
 while True:
     print('Ready to serve...')
     connectionSocket, addr = serverSocket.accept()
 
     try:
-        message = connectionSocket.recv(1024).decode()
+        message = connectionSocket.recv(1024).decode(errors='replace')
         filename = message.split()[1]
 
         f = open(filename[1:], encoding='utf-8')
@@ -25,14 +33,8 @@ while True:
         connectionSocket.send("\r\n".encode())
 
         connectionSocket.close()
-        serverSocket.close()
-        sys.exit()
 
     except IOError:
         connectionSocket.send("HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n".encode())
         connectionSocket.send("<html><body><h1>404 Not Found</h1></body></html>\r\n".encode())
         connectionSocket.close()
-        serverSocket.close()
-        sys.exit()
-
-# 1 client selesai -> server tutup
